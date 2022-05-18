f142cd40ce74b4d2ea6123363069e247977d1b24
Agustin  Coiro <agustinfcoiro@gmail.com>
2022-05-17 21:00:30 -0300

    agregando menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import ResponsiveAppBar from './components/NavBar/NavBar'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <ResponsiveAppBar/>
+
+
+        <p className="Titulo">
+          Editando <code>src/App.js</code> y agregando menú NavBar.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
     </div>
   );
 }
